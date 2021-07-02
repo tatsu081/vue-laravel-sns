1a4e721b6648767eb52bf7f24baf601ca67eb288
tatsu081 <kassi081dm@gmail.com>
2021-07-02 17:23:10 +0900

    #3 ログイン実装

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+Route::get('/', 'ArticleController@index')->name('articles.index'); //-- この行を編集
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth'); //-- この行を編集
