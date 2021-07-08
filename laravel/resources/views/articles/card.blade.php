155c6d1a3aeb873498163aed8d7ab818e5557a1f
tatsu081 <kassi081dm@gmail.com>
2021-07-08 15:16:02 +0900

    follow　フォロー機能
    1.フォロー機能実装

--- laravel/resources/views/articles/card.blade.php
+++ laravel/resources/views/articles/card.blade.php
@@ -4,7 +4,11 @@
             <i class="fas fa-user-circle fa-3x mr-1"></i>
         </a>
         <div>
-            <div class="font-weight-bold">{{ $article->user->name }}</div>
+            <div class="font-weight-bold">
+                <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
+                    {{ $article->user->name }}
+                </a>
+            </div>
             <div class="font-weight-lighter">{{ $article->created_at->format('Y/m/d H:i') }}</div>
         </div>
 
